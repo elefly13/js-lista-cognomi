61f9154319ab770d1c6055412128c7ccc3baa156
Elefly13 <elymara13@gmail.com>
2021-07-20 17:02:57 +0200

    Punto 3

--- js/main.js
+++ js/main.js
@@ -9,5 +9,10 @@ var lista_cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"] ;
 lista_cognomi.push(nuovo_cognome)
 
 console.log(lista_cognomi);
+
 // 3. stampa la lista ordinata alfabeticamente
+
+lista_cognomi.sort();
+console.log(lista_cognomi) ;
+
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
