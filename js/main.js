2427072b47f3c5fb8b638856a1c49a3aabdc96e6
Elefly13 <elymara13@gmail.com>
2021-07-20 16:51:05 +0200

    Punto 1 e Punto 2

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// 1. chiedi all’utente il cognome
+
+var nuovo_cognome = prompt("Inserisci il tuo cognome");
+
+
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+
+var lista_cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"] ;
+lista_cognomi.push(nuovo_cognome)
+
+console.log(lista_cognomi);
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
