5b05fbfc5ab6748709bd96cd0122e05ef380bda2
Elefly13 <elymara13@gmail.com>
2021-07-21 15:03:41 +0200

    Correzione esercizio

--- js/main.js
+++ js/main.js
@@ -6,22 +6,29 @@ var nuovo_cognome = prompt("Inserisci il tuo cognome");
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 
 var lista_cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"] ;
-lista_cognomi.push(nuovo_cognome)
+
+//correzione della prima lettera se inserita minuscola eminuscole tutte le altre lettere
+nuovo_cognome = nuovo_cognome.charAt(0).toUpperCase() + nuovo_cognome.slice(1).toLowerCase();
+
+lista_cognomi.push(nuovo_cognome);
+// var text2 = nuovo_cognome.toUpperCase();
 
 console.log(lista_cognomi);
 
 // 3. stampa la lista ordinata alfabeticamente
 
-lista_cognomi.sort();
+lista_cognomi.sort();//mette in ordine alfabetico
 console.log(lista_cognomi);
 
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+var lista_cognomiIndex = lista_cognomi.indexOf(nuovo_cognome);
+lista_cognomiIndex = lista_cognomiIndex + 1;
+
+console.log(lista_cognomiIndex);
 
 
 var i = 0;
 while (i < lista_cognomi.length) {
-    
-    console.log(lista_cognomi.indexOf(lista_cognomi[i]));
     document.getElementById("listaCognomi").innerHTML += "<li>" + lista_cognomi[i] + "</li>";
     i++;
 }
